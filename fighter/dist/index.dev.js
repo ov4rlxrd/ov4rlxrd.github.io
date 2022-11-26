6faf6725c46fc125ce8b5eda1e7892a7b9211ba6
ov4rlxrd <79939562+ov4rlxrd@users.noreply.github.com>
2022-11-26 15:58:33 +0300

    Add files via upload

---
+++ fighter/dist/index.dev.js
@@ -0,0 +1,319 @@
+"use strict";
+
+var _ref;
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
+var canvas = document.querySelector('canvas');
+var c = canvas.getContext('2d');
+canvas.width = 1024;
+canvas.height = 576;
+c.fillRect(0, 0, canvas.width, canvas.height);
+var gravity = 0.7;
+var background = new Sprite({
+  position: {
+    x: 0,
+    y: 0
+  },
+  imageSrc: '/fighter/gameAssets/oak_woods_v1.0/background.jpg'
+});
+var shop = new Sprite({
+  position: {
+    x: 600,
+    y: 138
+  },
+  imageSrc: '/fighter/gameAssets/oak_woods_v1.0/shop.png',
+  scale: 2.75,
+  framesMax: 6
+});
+var player = new Fighter({
+  position: {
+    x: 200,
+    y: 0
+  },
+  velocity: {
+    x: 0,
+    y: 0
+  },
+  imageSrc: '/fighter/gameAssets/oak_woods_v1.0/samurai/Martial Hero/Sprites/Idle.png',
+  framesMax: 8,
+  scale: 2.5,
+  offset: {
+    x: 215,
+    y: 150
+  },
+  sprites: {
+    idle: {
+      imageSrc: '/fighter/gameAssets/oak_woods_v1.0/samurai/Martial Hero/Sprites/Idle.png',
+      framesMax: 8
+    },
+    run: {
+      imageSrc: '/fighter/gameAssets/oak_woods_v1.0/samurai/Martial Hero/Sprites/Run.png',
+      framesMax: 8
+    },
+    jump: {
+      imageSrc: '/fighter/gameAssets/oak_woods_v1.0/samurai/Martial Hero/Sprites/Jump.png',
+      framesMax: 2
+    },
+    fall: {
+      imageSrc: '/fighter/gameAssets/oak_woods_v1.0/samurai/Martial Hero/Sprites/Fall.png',
+      framesMax: 2
+    },
+    attack1: {
+      imageSrc: '/fighter/gameAssets/oak_woods_v1.0/samurai/Martial Hero/Sprites/Attack1.png',
+      framesMax: 6
+    },
+    takeHit: {
+      imageSrc: '/fighter/gameAssets/oak_woods_v1.0/samurai/Martial Hero/Sprites/Take Hit - white silhouette.png',
+      framesMax: 4
+    },
+    death: {
+      imageSrc: '/fighter/gameAssets/oak_woods_v1.0/samurai/Martial Hero/Sprites/Death.png',
+      framesMax: 6
+    }
+  },
+  attackBox: {
+    offset: {
+      x: 75,
+      y: 50
+    },
+    width: 170,
+    height: 50
+  }
+});
+var enemy = new Fighter((_ref = {
+  position: {
+    x: 700,
+    y: 100
+  },
+  velocity: {
+    x: 0,
+    y: 0
+  },
+  color: 'blue',
+  offset: {
+    x: -50,
+    y: 0
+  },
+  imageSrc: '/fighter/gameAssets/oak_woods_v1.0/Martial hero 3/Sprite/Idle.png',
+  framesMax: 4,
+  scale: 2.5
+}, _defineProperty(_ref, "offset", {
+  x: 120,
+  y: 50
+}), _defineProperty(_ref, "sprites", {
+  idle: {
+    imageSrc: '/fighter/gameAssets/oak_woods_v1.0/Martial hero 3/Sprite/Idle.png',
+    framesMax: 10
+  },
+  run: {
+    imageSrc: '/fighter/gameAssets/oak_woods_v1.0/Martial hero 3/Sprite/Run.png',
+    framesMax: 8
+  },
+  jump: {
+    imageSrc: '/fighter/gameAssets/oak_woods_v1.0/Martial hero 3/Sprite/Going Up.png',
+    framesMax: 3
+  },
+  fall: {
+    imageSrc: '/fighter/gameAssets/oak_woods_v1.0/Martial hero 3/Sprite/Going Down.png',
+    framesMax: 3
+  },
+  attack1: {
+    imageSrc: '/fighter/gameAssets/oak_woods_v1.0/Martial hero 3/Sprite/Attack3.png',
+    framesMax: 9
+  },
+  takeHit: {
+    imageSrc: '/fighter/gameAssets/oak_woods_v1.0/Martial hero 3/Sprite/Take Hit.png',
+    framesMax: 3
+  },
+  death: {
+    imageSrc: '/fighter/gameAssets/oak_woods_v1.0/Martial hero 3/Sprite/Death.png',
+    framesMax: 11
+  }
+}), _defineProperty(_ref, "attackBox", {
+  offset: {
+    x: -115,
+    y: 80
+  },
+  width: 170,
+  height: 50
+}), _ref));
+var keys = {
+  a: {
+    pressed: false
+  },
+  d: {
+    pressed: false
+  },
+  w: {
+    pressed: false
+  },
+  ArrowLeft: {
+    pressed: false
+  },
+  ArrowRight: {
+    pressed: false
+  }
+};
+deacreaseTimer();
+
+function animate() {
+  window.requestAnimationFrame(animate);
+  c.fillStyle = 'black';
+  c.fillRect(0, 0, canvas.width, canvas.height);
+  background.update();
+  shop.update();
+  player.update();
+  enemy.update(); // Движение игрока
+
+  player.velocity.x = 0;
+
+  if (keys.a.pressed && player.lastKey === 'a') {
+    player.velocity.x = -5;
+    player.switchSprites('run');
+  } else if (keys.d.pressed && player.lastKey === 'd') {
+    player.velocity.x = 5;
+    player.switchSprites('run');
+  } else {
+    player.switchSprites('idle');
+  } // прыжок игрока
+
+
+  if (player.velocity.y < 0) {
+    player.switchSprites('jump');
+  } else if (player.velocity.y > 0) {
+    player.switchSprites('fall');
+  } // Движение противника
+
+
+  enemy.velocity.x = 0;
+
+  if (keys.ArrowLeft.pressed && enemy.lastKey === 'ArrowLeft') {
+    enemy.velocity.x = -5;
+    enemy.switchSprites('run');
+  } else if (keys.ArrowRight.pressed && enemy.lastKey === 'ArrowRight') {
+    enemy.velocity.x = 5;
+    enemy.switchSprites('run');
+  } else {
+    enemy.switchSprites('idle');
+  } // прыжок противника
+
+
+  if (enemy.velocity.y < 0) {
+    enemy.switchSprites('jump');
+  } else if (enemy.velocity.y > 0) {
+    enemy.switchSprites('fall');
+  } // соприкосновение
+
+
+  if (rectangularCollision({
+    rectangle1: player,
+    rectangle2: enemy
+  }) && player.isAttacking && player.framesCurrent === 4) {
+    enemy.takeHit();
+    player.isAttacking = false;
+    gsap.to('#enemyHealth', {
+      width: enemy.health + '%'
+    });
+  }
+
+  if (player.isAttacking && player.framesCurrent === 4) {
+    player.isAttacking == false;
+  }
+
+  if (rectangularCollision({
+    rectangle1: enemy,
+    rectangle2: player
+  }) && enemy.isAttacking && enemy.framesCurrent === 2) {
+    player.takeHit();
+    enemy.isAttacking = false;
+    gsap.to('#playerHealth', {
+      width: player.health + '%'
+    });
+  }
+
+  if (enemy.isAttacking && enemy.framesCurrent === 2) {
+    enemy.isAttacking == false;
+  } // Конец игры по хп
+
+
+  if (enemy.health <= 0 || player.health <= 0) {
+    determineWinner({
+      player: player,
+      enemy: enemy,
+      timerId: timerId
+    });
+  }
+}
+
+animate();
+window.addEventListener('keydown', function (event) {
+  if (!player.dead) {
+    switch (event.key) {
+      // Игрок
+      case 'd':
+        keys.d.pressed = true;
+        player.lastKey = 'd';
+        break;
+
+      case 'a':
+        keys.a.pressed = true;
+        player.lastKey = 'a';
+        break;
+
+      case 'w':
+        player.velocity.y = -20;
+        break;
+
+      case ' ':
+        player.attack();
+        break;
+    }
+  }
+
+  if (!enemy.dead) {
+    switch (event.key) {
+      case 'ArrowRight':
+        keys.ArrowRight.pressed = true;
+        enemy.lastKey = 'ArrowRight';
+        break;
+
+      case 'ArrowLeft':
+        keys.ArrowLeft.pressed = true;
+        enemy.lastKey = 'ArrowLeft';
+        break;
+
+      case 'ArrowUp':
+        enemy.velocity.y = -20;
+        enemy.lastKey = 'ArrowUp';
+        break;
+
+      case 'ArrowDown':
+        enemy.attack();
+        break;
+    }
+  }
+});
+window.addEventListener('keyup', function (event) {
+  // Игрок
+  switch (event.key) {
+    case 'd':
+      keys.d.pressed = false;
+      break;
+
+    case 'a':
+      keys.a.pressed = false;
+      break;
+  } // Противник
+
+
+  switch (event.key) {
+    case 'ArrowRight':
+      keys.ArrowRight.pressed = false;
+      break;
+
+    case 'ArrowLeft':
+      keys.ArrowLeft.pressed = false;
+      break;
+  }
+});
